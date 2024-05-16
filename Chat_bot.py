ac3293d13748756150c977c240491faffae8592c
unknown <kevalsaud25@gmail.com>
2024-05-16 22:26:05 +0530

    Update deploy v8

--- Chat_bot.py
+++ Chat_bot.py
@@ -39,13 +39,8 @@ PROMPT = PromptTemplate(input_variables=["chat_history", "input"], template=temp
 # Define the conversation chain based on the selected model
 if selected_model == 'Gemini by Google':
 
-    repo_id = "describeai/gemini"  #ids
 
-    llm = HuggingFaceEndpoint(
-    repo_id=repo_id, max_length=128, temperature=0.5, token=st.secrets['hugging-api']
-    )
-
-
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=st.secrets['gemini-api'])
     
 elif selected_model == 'Llama3 8B Parameter by Meta':
     HUGGINGFACEHUB_API_TOKEN = st.secrets['hugging-api']
