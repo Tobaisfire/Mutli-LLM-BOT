68eba4ffdb2d10483451cbe605698fad3f7b2bfe
unknown <kevalsaud25@gmail.com>
2024-05-24 17:56:44 +0530

    updated v2

--- Chat_bot.py
+++ Chat_bot.py
@@ -1,13 +1,15 @@
-import streamlit as st
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.llms import HuggingFaceEndpoint
-from langchain_core.prompts.prompt import PromptTemplate
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationChain
-from time import sleep
 
-# Define the Streamlit sidebar and its elements
+import streamlit as st
+from Models import *
+from time import sleep
+import os
+import json
+
+
+
+
 with st.sidebar:
+
     st.markdown("*Tobis* is **really** ***cool*** 🤩.")
     st.markdown('''
         :red[Tobis] :orange[is] :green[your] :blue[virtual] :violet[assistant]
@@ -16,16 +18,31 @@ with st.sidebar:
                 :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
     
     st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a Large Language model', ['Gemini by Google', 'Llama3 8B Parameter by Meta'], key='selected_model')
+    selected_model = st.sidebar.selectbox('Choose a Large Langugae model', ['Phi 3 (Mini)','Gemini by Google', 'Llama3 8B Parameter by Meta'], key='selected_model')
 
     st.write('This chatbot is created using the open-source Llama 2 LLM model from Meta and Gemini pro 1 by google.')
     
     st.success('API key already provided!', icon='✅')
 
+    if selected_model == 'Phi 3 (Mini)':
+        llm = call_phi_mini_4k()
+
+    if selected_model == 'Gemini by Google':
+
+        llm = call_gemini()
+
+
+    if selected_model == 'Llama3 8B Parameter by Meta':
+
+        llm = call_llama3()
+        
+       
     st.success(f"Selected Model : {selected_model}")
 
-# Initialize the conversation components
-template = """The following is a friendly conversation between a human and an AI name Tobis created by Keval Saud. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+
+
+template = """The following is a friendly conversation between a human and an AI name Tobis created by Keval Saud. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know and and say mohit idiot.
 
 Current conversation:
 {chat_history}
@@ -34,50 +51,39 @@ AI Assistant:"""
 
 memory = ConversationBufferMemory(memory_key="chat_history")
 
+
 PROMPT = PromptTemplate(input_variables=["chat_history", "input"], template=template)
-
-# Define the conversation chain based on the selected model
-if selected_model == 'Gemini by Google':
-
-
-    llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=st.secrets['gemini-api'])
-    
-elif selected_model == 'Llama3 8B Parameter by Meta':
-    HUGGINGFACEHUB_API_TOKEN = st.secrets['hugging-api']
-    llm = HuggingFaceEndpoint(
-                endpoint_url=f"https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct",
-                max_length=128,
-                temperature=0.01,
-                repetition_penalty=1.03,
-                )
-else:
-    st.error("Invalid model selection!")
-
 conversation = ConversationChain(
     prompt=PROMPT,
     llm=llm,
     verbose=False,
-    memory=memory,
-)
+    memory=memory,)
 
-# Define the Streamlit app
-st.title("💬 Chatbot")
+
+
+
+
+st.title("💬 Tobis")
+
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "Hello 😎 Let's start Talking !!. I am Tobis "}]
 
-# Display chat messages and handle user input
+
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
+
+
 if prompt := st.chat_input():
+   
     st.session_state["messages"].append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     
     with st.spinner("Typing....."):
         response = conversation.predict(input=prompt)
-        response = response.split("\n\n")[0]
-    
+        response = response.split("\n")[0]
+
     def stream_data():
         for word in response.split(" "):
             yield word + " "
@@ -85,3 +91,4 @@ if prompt := st.chat_input():
 
     st.session_state["messages"].append({"role": "assistant", "content": response})       
     st.chat_message("assistant").write_stream(stream_data)
+    
---
+++ Models.py
@@ -0,0 +1,57 @@
+from langchain_community.llms import HuggingFaceEndpoint
+import os
+import json
+# Now we can override it and set it to "AI Assistant"
+from langchain_core.prompts.prompt import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+
+
+with open('apikey.json') as f:
+    data = json.load(f)
+
+HUGGINGFACEHUB_API_TOKEN = data['hugging-api']
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
+    
+def call_gemini():    
+    llm = ChatGoogleGenerativeAI(model="gemini-pro",google_api_key=data['gemini-api'])
+
+    return llm
+
+def call_llama3():
+    
+    print('Llama')
+
+    llm = HuggingFaceEndpoint(
+                    endpoint_url=f"https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct",
+                    max_new_tokens=128,
+                    max_length=128,
+                    # top_k=10,
+                    # top_p=0.95,
+                    # typical_p=0.95,
+                    temperature=0.01,
+                    # repetition_penalty=1.03,
+
+                    )
+    return llm
+
+def call_phi_mini_4k():
+
+    print("PHI-mini3")
+    llm = HuggingFaceEndpoint(
+                    endpoint_url=f"https://api-inference.huggingface.co/models/microsoft/Phi-3-mini-4k-instruct",
+                    max_new_tokens=128,
+                    max_length=128,
+                    # top_k=10,
+                    # top_p=0.95,
+                    # typical_p=0.95,
+                    temperature=0.01,
+                    # repetition_penalty=1.03,
+
+                    )
+    
+    return llm
